78b45c27e08dcd129c393fc22714dba23a7a490b
absurdo-liver <6022537@cslaurentides.qc.ca>
2025-10-22 13:13:23 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const tipcalculatorBUTTON = document.addEventListener('click', () => { redirect('tipcalculator') });
+const temperatureconverterBUTTON = document.addEventListener('click', () => { redirect('temperatureconverter') });
+const testimonialsliderBUTTON = document.addEventListener('click', () => { redirect('testimonialslider') });
+const agecalculatorBUTTON = document.addEventListener('click', () => { redirect('agecalculator') });
+const recipebookappBUTTON = document.addEventListener('click', () => { redirect('recipebookapp') });
+const dicerollsimulatorBUTTON = document.addEventListener('click', () => { redirect('dicerollsimulator') });
+const rockpaperscissorsBUTTON = document.addEventListener('click', () => { redirect('rockpaperscissors') });
+const stopwatchBUTTON = document.addEventListener('click', () => { redirect('stopwatch') });
+const weightconverterBUTTON = document.addEventListener('click', () => { redirect('weightconverter') });
+const stepprogressbarBUTTON = document.addEventListener('click', () => { redirect('stepprogressbar') });
+const socialmediaselectorBUTTON = document.addEventListener('click', () => { redirect('socialmediaselector') });
+const realtimecharactercounterBUTTON = document.addEventListener('click', () => { redirect('realtimecharactercounter') });
+const randompasswordgeneratorBUTTON = document.addEventListener('click', () => { redirect('randompasswordgenerator') });
+
+
+function redirect(location){
+  window.location = 'https://absurdo-liver.github.io/projectIdeasFruition/' + location;
+}
