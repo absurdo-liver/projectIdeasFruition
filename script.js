b3624146e0389345a159b6816456c89c99494984
absurdo-liver <6022537@cslaurentides.qc.ca>
2025-10-22 15:30:26 -0400

    fixed critical mishap on my behalf

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-const tipcalculatorBUTTON = document.addEventListener('click', () => { redirect('tipcalculator') });
-const temperatureconverterBUTTON = document.addEventListener('click', () => { redirect('temperatureconverter') });
-const testimonialsliderBUTTON = document.addEventListener('click', () => { redirect('testimonialslider') });
-const agecalculatorBUTTON = document.addEventListener('click', () => { redirect('agecalculator') });
-const recipebookappBUTTON = document.addEventListener('click', () => { redirect('recipebookapp') });
-const dicerollsimulatorBUTTON = document.addEventListener('click', () => { redirect('dicerollsimulator') });
-const rockpaperscissorsBUTTON = document.addEventListener('click', () => { redirect('rockpaperscissors') });
-const stopwatchBUTTON = document.addEventListener('click', () => { redirect('stopwatch') });
-const weightconverterBUTTON = document.addEventListener('click', () => { redirect('weightconverter') });
-const stepprogressbarBUTTON = document.addEventListener('click', () => { redirect('stepprogressbar') });
-const socialmediaselectorBUTTON = document.addEventListener('click', () => { redirect('socialmediaselector') });
-const realtimecharactercounterBUTTON = document.addEventListener('click', () => { redirect('realtimecharactercounter') });
-const randompasswordgeneratorBUTTON = document.addEventListener('click', () => { redirect('randompasswordgenerator') });
+const tipcalculatorBUTTON = document.getElementById('tipcalculator'); 
+const temperatureconverterBUTTON = document.getElementById('temperatureconverter'); 
+const testimonialsliderBUTTON = document.getElementById('testimonialslider'); 
+const agecalculatorBUTTON = document.getElementById('agecalculator'); 
+const recipebookappBUTTON = document.getElementById('recipebookapp'); 
+const dicerollsimulatorBUTTON = document.getElementById('dicerollsimulator'); 
+const rockpaperscissorsBUTTON = document.getElementById('rockpaperscissors'); 
+const stopwatchBUTTON = document.getElementById('stopwatch'); 
+const weightconverterBUTTON = document.getElementById('weightconverter'); 
+const stepprogressbarBUTTON = document.getElementById('stepprogressbar'); 
+const socialmediaselectorBUTTON = document.getElementById('socialmediaselector'); 
+const realtimecharactercounterBUTTON = document.getElementById('realtimecharactercounter'); 
+const randompasswordgeneratorBUTTON = document.getElementById('randompasswordgenerator'); 
+
+tipcalculatorBUTTON.addEventListener('click',() => { redirect('tipcalculator'); });
+temperatureconverterBUTTON.addEventListener('click',() => { redirect('temperatureconverter'); });
+testimonialsliderBUTTON.addEventListener('click',() => { redirect('testimonialslider'); });
+agecalculatorBUTTON.addEventListener('click',() => { redirect('agecalculator'); });
+recipebookappBUTTON.addEventListener('click',() => { redirect('recipebookapp'); });
+dicerollsimulatorBUTTON.addEventListener('click',() => { redirect('dicerollsimulator'); });
+rockpaperscissorsBUTTON.addEventListener('click',() => { redirect('rockpaperscissors'); });
+stopwatchBUTTON.addEventListener('click',() => { redirect('stopwatch'); });
+weightconverterBUTTON.addEventListener('click',() => { redirect('weightconverter'); });
+stepprogressbarBUTTON.addEventListener('click',() => { redirect('stepprogressbar'); });
+socialmediaselectorBUTTON.addEventListener('click',() => { redirect('socialmediaselector'); });
+realtimecharactercounterBUTTON.addEventListener('click',() => { redirect('realtimecharactercounter'); });
+randompasswordgeneratorBUTTON.addEventListener('click',() => { redirect('randompasswordgenerator'); });
 
 
 function redirect(newlocation){
